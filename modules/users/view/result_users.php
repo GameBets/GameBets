7eb3807cc21ed1a895fff749dace435781436fef
Angel Conesa Camarena <ancoca1993@gmail.com>
2016-12-05 21:06:26 +0100

    remove view non necessary

--- modules/users/view/result_users.php
+++
@@ -1,5 +0,0 @@
-<link rel="stylesheet" href="<?php echo CSS_USERS_PATH ?>users.css" type="text/css">
-
-<div id="content"></div>
-
-<script type="text/javascript" src="<?php echo JS_USERS_PATH ?>result_users.js" ></script>
