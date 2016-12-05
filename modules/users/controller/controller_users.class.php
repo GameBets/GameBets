5a8fdaa5afb11aeab2d28db6616992ce70853468
Angel Conesa Camarena <ancoca1993@gmail.com>
2016-12-05 20:51:01 +0100

    functions of controller for signup

--- modules/users/controller/controller_users.class.php
+++ modules/users/controller/controller_users.class.php
@@ -4,87 +4,230 @@
         public function __construct() {
         	include(UTILS_USERS_PATH . "utils_users.inc.php");
         	include(UTILS_PATH . "upload.php");
+          include (LIBS . 'password_compat-master/lib/password.php');
 
           $_SESSION['module'] = "users";
         }
 
-        public function form_users() {
-            loadView('modules/users/view/', 'create_users.php');
+/////////////SIGNUP/////////////////////////////////////////////////////////////
+
+        public function signup() {
+            loadView('modules/users/view/', 'signup.php');
         }
 
-        public function result_users() {
-            loadView('modules/users/view/', 'result_users.php');
-        }
-
-        public function alta_users(){
+        public function signup_users(){
           //Si hay datos del formulario en el json enviado por el controlador de javascript
-        	if(isset($_POST['create_users'])){
+        	if(isset($_POST['alta_users'])){
           //Validamos los datos correctamente
           //Si los datos estan correctos, los guardamos y devolvemos un json con el resultado
           //Silos datos no estan correctos, devolvemos el resultado y los errores en un json
         		$jsondata = array();
-        		$usersJSON = json_decode($_POST["create_users"], true);
+        		$usersJSON = json_decode($_POST["alta_users"], true);
 
-        		$result = validate_users($usersJSON); //Validamos los datos
-        		if (empty($_SESSION['result_avatar'])){ //Comprobamos si hay imagen en el dropzone, sino utilizamos la de por defecto
-        			$_SESSION['result_avatar'] = array('resultado' => true, 'error' =>"", 'datos' => 'media/default-results.png');
-        		}
+        		$result = validate_signup($usersJSON); //Validamos los datos
 
+        		$avatar = get_gravatar($result['datos']['email'], $s = 400, $d = 'identicon', $r = 'g', $img = false, $atts = array());
+            $_SESSION['result_avatar'] = array('resultado' => true, 'error' =>"", 'datos' => $avatar);
         		$result_avatar = $_SESSION['result_avatar'];
 
         		if ($result['resultado'] && $result_avatar['resultado']) { //Guardamos los datos si el resultado es positivo
         				$arrArgument = array(
-        					'name_user' => $result['datos']['name_user'],
-        					'password' => $result['datos']['password'],
+        					'password' => password_hash($result['datos']['password'], PASSWORD_BCRYPT),
         					'avatar' => $result_avatar['datos'],
-        					'name' => $result['datos']['name'],
-        					'surname' => $result['datos']['surname'],
-        					'date_birthday' => $result['datos']['date_birthday'],
         					'email' => $result['datos']['email'],
-        					'phone' => $result['datos']['phone'],
-        					'country' => $result['datos']['country'],
-        					'province' => $result['datos']['province'],
-        					'town' => $result['datos']['town'],
+                  'type' => "client",
+                  'active' => 0,
+                  'token' => ""
         				);
 
-                /////////////////insert into BD////////////////////////
-                $arrValue = false;
+                /* Control de registro */
+                set_error_handler('ErrorHandler');
                 try {
-                  $arrValue = loadModel(MODEL_USERS_PATH, "users_model", "create_users", $arrArgument);
+                    //loadModel
+                    $arrValue = loadModel(MODEL_USERS_PATH, "users_model", "count", array('column' => array('email'), 'like' => array($arrArgument['email'])));
+                    if ($arrValue[0]['total'] == 1) {
+                        $arrValue = false;
+                        $result['error']['email'] = "Email ya registrado";
+                    }
                 } catch (Exception $e) {
-                  showErrorPage(2, "ERROR - 503 BD", 'HTTP/1.0 503 Service Unavailable', 503);
+                    $arrValue = false;
                 }
+                restore_error_handler();
+                /* Fin de control de registro */
 
-                if ($arrValue)
-                    $mensaje = "Su registro se ha efectuado correctamente, para finalizar compruebe que ha recibido un correo de validacion y siga sus instrucciones";
-                else
-                    $mensaje = "No se ha podido realizar su alta. Intentelo mas tarde";
+                if($arrValue) {
+                    set_error_handler('ErrorHandler');
+                    try {
+                    $arrArgument['token'] = md5(uniqid(rand(), true));
+                      $arrValue = loadModel(MODEL_USERS_PATH, "users_model", "signup", $arrArgument);
+                    } catch (Exception $e) {
+                      showErrorPage(2, "ERROR - 503 BD", 'HTTP/1.0 503 Service Unavailable', 503);
+                    }
+                    restore_error_handler();
 
-        				//redirigir a otra pagina con los datos de $arrArgument y $mensaje
-        				$_SESSION['users'] = $arrArgument;
-        				$_SESSION['msje'] = $mensaje;
-        				$callback="../../users/result_users/";
+                    if ($arrValue){
+                        send_email($arrArgument, "alta");
+                        $mensaje = "Su registro se ha efectuado correctamente, para finalizar compruebe que ha recibido un correo de validacion y siga sus instrucciones";
+                    }else{
+                        $mensaje = "No se ha podido realizar su alta. Intentelo mas tarde";
+                    }
 
-        				$jsondata["success"] = true;
-        				$jsondata["redirect"] = $callback;
-        				echo json_encode($jsondata);
-        				exit;
+            				//redirigir a otra pagina con los datos de $arrArgument y $mensaje
+            				$_SESSION['users'] = $arrArgument;
+            				$_SESSION['msje'] = $mensaje;
+
+            				$jsondata["success"] = true;
+                    $jsondata['msje'] = $mensaje;
+            				echo json_encode($jsondata);
+                    exit;
+                } else {
+                  $jsondata["success"] = false;
+                  $jsondata["error"] = $result['error'];
+                  echo json_encode($jsondata);
+                  exit;
+                }
         		} else {  //Devolvemos los errores si el resultado es negativo
-        				//$error = $result['error'];
-        				//$error_image = $result_image['error'];
         				$jsondata["success"] = false;
         				$jsondata["error"] = $result['error'];
-        				$jsondata["error_avatar"] = $result_avatar['error'];
-
-        				$jsondata["success_avatar"] = false;
-        				if ($result_avatar['resultado']) {
-        						$jsondata["success_avatar"] = true;
-        						$jsondata["img_users"] = $result_avatar['datos'];
-        				}
         				echo json_encode($jsondata);
         				exit;
         		}
         	}
+        }
+
+        public function active() {
+            if ($_GET['aux']) {
+                $arrArgument = array(
+                    'column' => array('token'),
+                    'like' => array($_GET['aux']),
+                    'field' => array('active'),
+                    'new' => array('1')
+                );
+                set_error_handler('ErrorHandler');
+                try {
+                    $value = loadModel(MODEL_USERS_PATH, "users_model", "update", $arrArgument);
+                } catch (Exception $e) {
+                    $value = false;
+                }
+                restore_error_handler();
+
+                if ($value) {
+                    loadView('modules/users/view/', 'active.php');
+                } else {
+                    showErrorPage(4, "", 'HTTP/1.0 503 Service Unavailable', 503);
+                }
+            }
+        }
+
+/////////////END SIGNUP/////////////////////////////////////////////////////////
+
+        public function update_users(){
+          //Si hay datos del formulario en el json enviado por el controlador de javascript
+          if(isset($_POST['create_users'])){
+          //Validamos los datos correctamente
+          //Si los datos estan correctos, los guardamos y devolvemos un json con el resultado
+          //Silos datos no estan correctos, devolvemos el resultado y los errores en un json
+            $jsondata = array();
+            $usersJSON = json_decode($_POST["create_users"], true);
+
+            $result = validate_users($usersJSON); //Validamos los datos
+            if (empty($_SESSION['result_avatar'])){ //Comprobamos si hay imagen en el dropzone, sino utilizamos la de por defecto
+              $avatar = get_gravatar($result['datos']['email'], $s = 400, $d = 'identicon', $r = 'g', $img = false, $atts = array());
+              $_SESSION['result_avatar'] = array('resultado' => true, 'error' =>"", 'datos' => $avatar);
+            }
+
+            $result_avatar = $_SESSION['result_avatar'];
+
+            if ($result['resultado'] && $result_avatar['resultado']) { //Guardamos los datos si el resultado es positivo
+                $arrArgument = array(
+                  'name_user' => $result['datos']['name_user'],
+                  'password' => $result['datos']['password'],
+                  'avatar' => $result_avatar['datos'],
+                  'name' => $result['datos']['name'],
+                  'surname' => $result['datos']['surname'],
+                  'date_birthday' => $result['datos']['date_birthday'],
+                  'email' => $result['datos']['email'],
+                  'phone' => $result['datos']['phone'],
+                  'country' => $result['datos']['country'],
+                  'province' => $result['datos']['province'],
+                  'town' => $result['datos']['town'],
+                  'token' => ""
+                );
+
+                /* Control de registro */
+                set_error_handler('ErrorHandler');
+                try {
+                    //loadModel
+                    $arrValue = loadModel(MODEL_USERS_PATH, "users_model", "count", array('column' => array('name_user'), 'like' => array($arrArgument['name_user'])));
+                    if ($arrValue[0]['total'] == 1) {
+                        $arrValue = false;
+                        $result['error']['name_user'] = "Nombre de usuario no disponible";
+                    } else {
+                        $arrValue = loadModel(MODEL_USERS_PATH, "users_model", "count", array('column' => array('email'), 'like' => array($arrArgument['email'])));
+                        if ($arrValue[0]['total'] == 1) {
+                            $arrValue = false;
+                            $result['error']['email'] = "Email ya registrado";
+                        }
+                    }
+                } catch (Exception $e) {
+                    $arrValue = false;
+                }
+                restore_error_handler();
+                /* Fin de control de registro */
+
+                if($arrValue) {
+                    set_error_handler('ErrorHandler');
+                    try {
+                    $arrArgument['token'] = md5(uniqid(rand(), true));
+                      $arrValue = loadModel(MODEL_USERS_PATH, "users_model", "create_users", $arrArgument);
+                    } catch (Exception $e) {
+                      showErrorPage(2, "ERROR - 503 BD", 'HTTP/1.0 503 Service Unavailable', 503);
+                    }
+                    restore_error_handler();
+
+                    if ($arrValue){
+                        send_email($arrArgument, "alta");
+                        $mensaje = "Su registro se ha efectuado correctamente, para finalizar compruebe que ha recibido un correo de validacion y siga sus instrucciones";
+                    }else{
+                        $mensaje = "No se ha podido realizar su alta. Intentelo mas tarde";
+                    }
+
+                    //redirigir a otra pagina con los datos de $arrArgument y $mensaje
+                    $_SESSION['users'] = $arrArgument;
+                    $_SESSION['msje'] = $mensaje;
+                    $callback="../../users/result_users/";
+
+                    $jsondata["success"] = true;
+                    $jsondata["redirect"] = $callback;
+                    echo json_encode($jsondata);
+                    exit;
+                } else {
+                  $jsondata["success"] = false;
+                  $jsondata["error"] = $result['error'];
+                  $jsondata["error_avatar"] = $result_avatar['error'];
+
+                  $jsondata["success_avatar"] = false;
+                  if ($result_avatar['resultado']) {
+                      $jsondata["success_avatar"] = true;
+                      $jsondata["img_users"] = $result_avatar['datos'];
+                  }
+                  echo json_encode($jsondata);
+                  exit;
+                }
+            } else {  //Devolvemos los errores si el resultado es negativo
+                $jsondata["success"] = false;
+                $jsondata["error"] = $result['error'];
+                $jsondata["error_avatar"] = $result_avatar['error'];
+
+                $jsondata["success_avatar"] = false;
+                if ($result_avatar['resultado']) {
+                    $jsondata["success_avatar"] = true;
+                    $jsondata["img_users"] = $result_avatar['datos'];
+                }
+                echo json_encode($jsondata);
+                exit;
+            }
+          }
         }
 
         public function upload_avatar_users() {
@@ -164,31 +307,6 @@
         		}else{
         			$json = "error";
         			echo $json;
-        			exit;
-        		}
-        	}
-        }
-
-        public function load_pepi_users() {
-          echo json_encode("HOLA");
-          exit;
-        	if(  (isset($_POST["load_provincias"])) && ($_POST["load_provincias"] == true)  ){
-        		$jsondata = array();
-            $json = array();
-
-            try {
-              $json = loadModel(MODEL_USERS_PATH, "users_model", "obtain_provincias");
-            } catch (Exception $e) {
-              $json = array();
-            }
-
-        		if($json){
-        			$jsondata["provincias"] = $json;
-        			echo json_encode($jsondata);
-        			exit;
-        		}else{
-        			$jsondata["provincias"] = "error";
-        			echo json_encode($jsondata);
         			exit;
         		}
         	}
