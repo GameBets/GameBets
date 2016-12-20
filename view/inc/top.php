566b61707f0e48a3426490d1e926771027ac775f
Luis Donat <donatgirones@gmail.com>
2016-12-20 01:50:03 +0100

    Inserted Favicon

--- view/inc/top.php
+++ view/inc/top.php
@@ -14,6 +14,13 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 <!--fonts-->
 <link href='http://fonts.googleapis.com/css?family=Montserrat+Alternates:400,700' rel='stylesheet' type='text/css'>
 <link href='http://fonts.googleapis.com/css?family=PT+Sans:400,700' rel='stylesheet' type='text/css'>
+<!--favicon-->
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo IMAGES_PATH ?>apple-touch-icon.png">
+<link rel="icon" type="image/png" href="<?php echo IMAGES_PATH ?>favicon-32x32.png" sizes="32x32">
+<link rel="icon" type="image/png" href="<?php echo IMAGES_PATH ?>favicon-16x16.png" sizes="16x16">
+<link rel="manifest" href="<?php echo IMAGES_PATH ?>manifest.json">
+<link rel="mask-icon" href="<?php echo IMAGES_PATH ?>safari-pinned-tab.svg" color="#5bbad5">
+<meta name="theme-color" content="#ffffff">
 <!--modernizer-->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.6.2/modernizr.js"></script>
 <link rel="stylesheet" type="text/css" href="<?php echo CSS_PATH ?>component.css" />
