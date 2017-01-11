3466a79b374fc820611ea35acd812a00193e8337
Luis Donat <donatgirones@gmail.com>
2017-01-11 21:03:52 +0100

    Adapt to PHP7 function inputfilter

--- backend/libs/inputfilter.class.php
+++ backend/libs/inputfilter.class.php
@@ -270,19 +270,21 @@ class inputfilter {
      */
     function decode($source) {
       // url decode
-      $source = html_entity_decode($source, ENT_QUOTES, 'ISO-8859-1');
-      // convert decimal
-      // $source = preg_replace('/&#(\d+);/me', "chr(\\1)", $source);    // decimal notation
-      $source = preg_replace_callback('/&#(\d+);/m',
-        function ($matches) {return $this->chr($matches[1]);},
-        $source
-      );
-      // convert hex
-      // $source = preg_replace('/&#x([a-f0-9]+);/mei', "chr(0x\\1)", $source); // hex notation
-      $source = preg_replace_callback('/&#x([a-f0-9]+);/mi',
-        function ($matches) {return $this->chr($matches[1]);},
-        $source
-      );
+      
+        $source = html_entity_decode($source, ENT_QUOTES, 'ISO-8859-1');
+        // convert decimal
+        // $source = preg_replace('/&#(\d+);/me', "chr(\\1)", $source);    // decimal notation
+        $source = preg_replace_callback('/&#(\d+);/m',
+          function ($matches) {return $this->chr($matches[1]);},
+          $source
+        );
+        // convert hex
+        // $source = preg_replace('/&#x([a-f0-9]+);/mei', "chr(0x\\1)", $source); // hex notation
+        $source = preg_replace_callback('/&#x([a-f0-9]+);/mi',
+          function ($matches) {return $this->chr($matches[1]);},
+          $source
+        );
+
         return $source;
     }
 
