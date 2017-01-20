bac675e1defa75a6a255f5a4587e3ea6e7840f30
Luis Donat <donatgirones@gmail.com>
2017-01-20 16:47:13 +0100

    Insert frontend on path

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', ['ngRoute'/*, 'ngAnimate', 'ui.bootstrap', 'ngCookies', 'facebook'*/]);
+var app = angular.module('myApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap'/*, 'ngCookies', 'facebook'*/]);
 
 app.config(['$routeProvider',
     function ($routeProvider) {
