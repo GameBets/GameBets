867081e6aa82f424be0245f118a458fba2b3337d
Luis Donat <donatgirones@gmail.com>
2017-02-07 17:48:11 +0100

    add login backend functionality

---
+++ src/server/login/login.controller.js
@@ -0,0 +1,20 @@
+
+var passport = require('passport');
+
+exports.signup = function (req, res, next) {
+    passport.authenticate('local-signup', function (err, user, info) {
+        if (err) {
+            return res.send("err");
+        }
+        if (!user) {
+            return res.send("name");
+        }
+        return res.send(true);
+
+    })(req, res, next);
+
+};
+
+
+
+
