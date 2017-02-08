9bd88edf551bb2220af1ee3a2dcd698aa2a0a99f
Danny <dapriad@gmail.com>
2017-02-08 20:02:27 +0100

    cambios en respuesta del servidor por si el usuario existe

--- src/server/modules/users/controller/user.js
+++ src/server/modules/users/controller/user.js
@@ -1,14 +1,14 @@
 var passport = require('passport');
 
 exports.signup = function (req, res, next) {
-  console.log(req.body);
-  console.log('entra al signup');
+  // console.log(req.body);
+  // console.log('entra al signup');
     passport.authenticate('local-signup', function (err, user, info) {
         if (err) {
             return res.send('err');
         }
         if (!user) {
-            return res.send(req.body);
+            return res.send('Usuario ya existe');
         }
         return res.send(true);
 
