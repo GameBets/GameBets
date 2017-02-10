0781f625166fd56c4180f3451b2f5a61dc67c2ef
Luis Donat <donatgirones@gmail.com>
2017-02-10 17:41:28 +0100

    new functions on passport for facebook

---
+++ src/server/config/auth.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'facebookAuth' : {
+        'clientID'        : '654724108022675', // your App ID
+        'clientSecret'    : '7987787bf953d8419d4c02873e80eeb6', // your App Secret
+        'callbackURL'     : 'http://localhost:3000/auth/facebook/callback'
+    },
+  };
--- src/server/config/passport.js
+++ src/server/config/passport.js
@@ -1,6 +1,11 @@
 var LocalStrategy = require('passport-local').Strategy;
 var bcrypt = require('bcrypt-nodejs');
 var modeloUsuarios = require('./../modules/login/login.model');
+var passport = require('passport');
+var confAuth = require('./auth'); // use this one for testing
+var LocalStrategy = require('passport-local').Strategy;
+//var TwitterStrategy = require('passport-twitter').Strategy;
+var FacebookStrategy = require('passport-facebook').Strategy;
 
 //exportamos lalibreria de funciones
 module.exports = function(passport) {
@@ -17,17 +22,21 @@ module.exports = function(passport) {
   //Passport serializará y deserializará las instancias de usuario de la sesión.
 
   passport.serializeUser(function(user, done) {
-    console.log('uso serializer'); //no borrar
-    done(null, user.id);
+    done(null, user);
   });
 
+  passport.deserializeUser(function(user, done) {
+    done(null, user);
+  });
+
+  /*
   // used to deserialize the user
   passport.deserializeUser(function(id, done) {
     console.log('uso deserialize'); //no borrar
     mysql.connection.query('SELECT * FROM user_test WHERE id =' + id, function(err, rows) {
       done(err, rows[0]);
     });
-  });
+  });*/
 
   //En este ejemplo, sólo el ID de usuario se serializa en la sesión, manteniendo
   //pequeña la cantidad de datos almacenados dentro de la sesión. Cuando se reciben
@@ -72,4 +81,51 @@ module.exports = function(passport) {
           } //fin del else
         }); //fin de count
       })); //fin de local   )
+
+
+  /////////////////////// FacebookStrategy
+
+  passport.use(new FacebookStrategy({
+      clientID: confAuth.facebookAuth.clientID,
+      clientSecret: confAuth.facebookAuth.clientSecret,
+      callbackURL: confAuth.facebookAuth.callbackURL,
+      profileFields: ['name', 'email', 'link', 'locale', 'timezone'],
+      passReqToCallback: true
+    },
+    function (req, accessToken, refreshToken, profile, done) {
+
+           modeloUsuarios.countUser_Face(profile.id, function (rows) {
+               if (rows[0].userCount === 0) {
+
+                   console.log('no existe e inserto');
+                   var newUser = {
+                       username: profile.id,
+                       email: profile._json.email,
+                       usertype: 'client',
+                       passwd:''
+                   };
+                   console.log(newUser);
+                   modeloUsuarios.insertUser(newUser, function (rows) {
+                       if (rows) {
+                           return done(null, rows);
+                       }
+                   });//fin de consulta
+                   return done(null, rows);
+               } else {
+                   console.log('si existe y devuelvo datos');
+                   modeloUsuarios.getUser(profile.id, function (error, rows) {
+                       if (!rows.length) {
+
+                           return done(null, false, 'nouser');
+
+                       } else {
+                          console.log(rows[0]);
+                           return done(null, rows[0]);
+                       }
+                   });
+
+               }//fin del else
+           });//fin de count
+ }));
+
 };
