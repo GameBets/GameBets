b196a53b35d86ce33d7f09aea4f8a34cb5759dd8
Luis Donat <donatgirones@gmail.com>
2017-02-10 17:39:04 +0100

    controller facebook server

--- src/server/modules/login/login.controller.js
+++ src/server/modules/login/login.controller.js
@@ -1,17 +1,59 @@
 var passport = require('passport');
+var LoginController = {};
 
-exports.signup = function (req, res, next) {
+LoginController.signup = function(req, res, next) {
   // console.log(req.body);
   // console.log('entra al signup');
-    passport.authenticate('local-signup', function (err, user, info) {
+  passport.authenticate('local-signup', function(err, user, info) {
+    if (err) {
+      return res.send('err');
+    }
+    if (!user) {
+      return res.send('Usuario ya existe');
+    }
+    return res.send(true);
+
+  })(req, res, next);
+
+};
+
+
+
+
+LoginController.facebook = function (req, res, next) {
+
+    passport.authenticate('facebook', function (err, user, info) {
+
         if (err) {
             return res.send('err');
         }
         if (!user) {
-            return res.send('Usuario ya existe');
+            return res.send('errorcredentials');
         }
-        return res.send(true);
-
+        //return res.send(user);
+        res.redirect('/');
     })(req, res, next);
 
 };
+
+
+
+
+
+// authenticate logger
+LoginController.logger = function(req, res) {
+  console.log('Logged in EXPRESS' + JSON.stringify(req.user));
+  res.send(req.isAuthenticated() ? req.user : '0');
+};
+
+
+
+
+// logout
+LoginController.logout = function(req, res) {
+  req.logOut();
+  res.redirect('/');
+  //res.send(200);
+};
+
+module.exports = LoginController;
