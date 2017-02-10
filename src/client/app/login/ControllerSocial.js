14c8038b395a6b801645ad0ba8edd65cbbc10639
Luis Donat <donatgirones@gmail.com>
2017-02-10 17:40:35 +0100

    controller for facebook social

---
+++ src/client/app/login/ControllerSocial.js
@@ -0,0 +1,24 @@
+(function () {
+    'use strict';
+
+    angular
+            .module('app.login')
+            .controller('ControllerSocial', LoginController);
+
+    LoginController.$inject = ['dataservice', '$state', '$timeout', 'logger'];
+
+    function LoginController(dataservice, $state, $timeout, logger) {
+        var vm = this;
+
+        social();
+
+        function social() {
+
+            dataservice.facebook().then(function (response) {
+                console.log(response);
+                logger.success('Usuario autentificado');
+                $state.go('dashboard');
+            });
+        }
+    }
+})();
