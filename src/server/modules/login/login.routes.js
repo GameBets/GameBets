cc51815dbe754f32919d4c473b07bdd6e7aced5f
Luis Donat <donatgirones@gmail.com>
2017-02-13 00:03:42 +0100

    delete old functions

--- src/server/modules/login/login.routes.js
+++ src/server/modules/login/login.routes.js
@@ -6,29 +6,15 @@ var loginController = require('./login.controller');
 
 router.post('/users_signup', loginController.signup);
 
-
-// route to test if the user is logged in or not
-router.get('/loggedin', loginController.logger);
-
 router.get('/auth/facebook', loginController.facebook);
 
 router.get('/auth/facebook/callback', loginController.facebookCallback);
 
 router.get('/auth/twitter', loginController.twitter);
 router.get('/auth/twitter/callback', loginController.twitterCallback);
-/*
-app.get('/auth/google', passport.authenticate('google', {
-  scope: 'profile email'
-}));
-app.get('/auth/google/callback', passport.authenticate('google', {
-  successRedirect: '/socialsignin',
-  failureRedirect: '/'
-}));
-*/
+
 //retorno del cliente para recoger los datos
 router.get('/auth/success', loginController.auth);
-
-
 // route to log out
 router.get('/logout', loginController.logout);
 
