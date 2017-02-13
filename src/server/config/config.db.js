cde7c2775e49f27a37bb79dbba1278110bb7ffd5
Danny <dapriad@gmail.com>
2017-02-13 05:48:58 +0100

    Merge signin/signup local

--- src/server/config/config.db.js
+++ src/server/config/config.db.js
@@ -2,7 +2,7 @@ var mysql = require('mysql');
 // console.log("holaaa");
   exports.connection = mysql.createConnection({
       host: '127.0.0.1',
-      user: 'daniel',
-      password: '',
+      user: 'root',
+      password: 'root',
       database: 'GameBets_DB'
   });
