645dc3e4721f21bf88b3c3e208f72318da2be43d
Luis Donat <donatgirones@gmail.com>
2017-02-13 03:54:29 +0100

    create profile module

---
+++ src/client/app/profile/profile.controller.js
@@ -0,0 +1,21 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.profile')
+    .controller('ProfileController', ProfileController);
+
+  ProfileController.$inject = ['logger'];
+  /* @ngInject */
+  function ProfileController(logger) {
+    var vm = this;
+    vm.title = 'Profile';
+//    vm.ClickMeToRedirect = ClickMeToRedirect;
+
+    activate();
+
+    function activate() {
+      logger.info('Activated Profile View');
+    }
+  }
+})();
