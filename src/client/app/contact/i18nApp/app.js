7f671c2ed57a3376b09f561d6337bf5fca7d2cfc
Luis Donat <donatgirones@gmail.com>
2017-02-14 07:29:00 +0100

    add add js to lang

---
+++ src/client/app/contact/i18nApp/app.js
@@ -0,0 +1,35 @@
+  /* 'use strict';
+
+
+  var app =  angular
+    .module('app.contact', ['pascalprecht.translate']);
+
+  app.config(function($translateProvider) {
+    $translateProvider.fallbackLanguage('en');
+    $translateProvider.registerAvailableLanguageKeys(['en', 'es'], {
+      'en_*': 'en',
+      'es_*': 'es'
+    })
+
+    $translateProvider.translations('en', {
+      contact: "CONTACT",
+      required: "This field is required",
+      valid_email: "Email not valid",
+      support: "SUPPORT",
+      technical: "TECHNICAL SUPPORT",
+      documentation: "DOCUMENTATION"
+
+    });
+    $translateProvider.translations('es', {
+      contact: "CONTACTO",
+      required: "El campo es obligatorio",
+      valid_email: "El email no es valido",
+      support: "SOPORTE",
+      technical: "SOPORTE TECNICO",
+      documentation: "DOCUMENTACION"
+    });
+
+    $translateProvider.useSanitizeValueStrategy('escape');
+    $translateProvider.preferredLanguage('en');
+
+  });*/
